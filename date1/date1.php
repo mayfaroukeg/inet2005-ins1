1f047a9c0a31e57a43c777b45139ac7771cf1068
mayfaroukeg <mayfaroukeg@gmail.com>
2016-09-14 15:56:38 -0300

    14-9 class demo3

---
+++ date1/date1.php
@@ -0,0 +1,14 @@
+
+<?php
+    echo "<!DOCTYPE html>”;
+    echo "<html>";
+    echo "<head>";
+    echo "<title>Today's Date now</title>";
+    echo "</head>";
+    echo "<body>";
+    echo "<h3>Today is now ";
+    echo date("Y/m/d");
+    echo ".</h3>";
+    echo "</body>";
+    echo "</html>";
+?>
