2870c66b7cd03b1179f3b06bbe323bcc1cd041f9
mayfaroukeg <mayfaroukeg@gmail.com>
2016-09-14 10:39:36 -0300

    demosep12 in class

---
+++ sept12Demo/index.php
@@ -0,0 +1,20 @@
+<h1>Hello from Mik's php</h1>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inet2005
+ * Date: 9/14/16
+ * Time: 10:21 AM
+ */
+echo "<p> praragraph text</p>";
+?>
+
+<h2> not a good ideaphp</h2>
+
+<?php
+$message="Hello";
+$target="world";
+
+echo "<h5> $message, $target</h5>"
+?>
