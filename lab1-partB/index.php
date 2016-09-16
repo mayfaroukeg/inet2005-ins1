12d296bc84580fbb112c5a76215b783b26eacbb8
mayfaroukeg <mayfaroukeg@gmail.com>
2016-09-16 15:14:43 -0300

    lab1-version

---
+++ lab1-partB/index.php
@@ -0,0 +1,88 @@
+
+<body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inet2005
+ * Date: 9/16/16
+ * Time: 2:39 PM
+ */
+
+
+echo ("<h1> Greetings from lab 1!!</h1>");
+
+?>
+
+<p> This is some text in a paraghraph</p>
+
+
+<?php
+
+
+echo ("<h3> This is h3 heading!!</h3>");
+
+?>
+
+
+<?php
+
+ $name = "May";
+  echo $name
+
+
+?>
+<p>
+
+<?php
+$string1 = "the sky is  ";
+$string2 = "blue";
+
+$result = $string1 . $string2;
+
+echo $result
+
+?>
+<p>
+
+<?php
+
+$val1=32;
+$val2= 14;
+$val3=83;
+
+$result2= ($val1*$val2)+$val3;
+
+
+echo $result2;
+
+?>
+</p>
+
+<p>
+<?php
+
+$val4=1024;
+$val5=128;
+$val6=7;
+
+$result3= ($val4/$val5)-$val6;
+
+echo $result3;
+?>
+
+    </p>
+
+<p>
+
+    <?Php
+    $val7=769;
+    $val8=6;
+
+   $result4= $val7 % $val8;
+    echo $result4;
+
+?>
+
+</p>
+</body>
