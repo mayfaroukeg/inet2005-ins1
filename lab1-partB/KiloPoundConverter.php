feffcc6a7e64d29976921dcc3481d710d165dc47
mayfaroukeg <mayfaroukeg@gmail.com>
2016-09-19 07:52:56 -0300

    kilo pound converter 1

--- lab1-partB/KiloPoundConverter.php
+++ lab1-partB/KiloPoundConverter.php
@@ -1,36 +1,39 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
-<title>Kilo Pound Converter</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+	<title>Kilo Pound Converter</title>
 </head>
 <body>
-  		<h1>Results:</h1>
-        <?php
-			$poundsResult = 0;
-			$kilosResult = 0;
-		
-		    if (isset($_POST['ConverttoPounds']))
-		    {
-		        $kilosResult = $_POST['kilos'];
-		        
-		        $poundsResult = $kilosResult * 2.2;
-		        
-		        echo "<p>" + $kilosResult + " kilos equals " + $poundResult + " pounds.</p>";
-		    }
-		    elseif (!isset($_POST['ConverttoKilos']))
-		    {
-		    	$poundsResult = $_POST['pound'];
-		    
-		    	$kilosResult = $poundsResult / 2.2;
-		    	
-		    	echo "<p>" + $poundsResult + " pounds equals " + $kilosResult + " kilos.</p>";
-		    }
-		    else
-		    {
-		    	echo "<p>Nothing to do.</p>";
-		    }
-		?>
-		<p><a href="KiloPoundForm.html">Go back.</a></p>
+<h1>Results:</h1>
+<?php
+$poundsResult = 0;
+$kilosResult = 0;
+
+if (isset($_POST['pounds']) && $_POST['pounds']!='')
+{
+
+	$poundsResult = $_POST['pounds'];
+
+	$kilosResult = $poundsResult / 2.2;
+
+	echo "<p>" . $poundsResult . " pounds equals " . $kilosResult . " kilos.</p>";
+
+}
+elseif (isset($_POST['kilos']) && $_POST['kilos']!='' )
+{
+
+	$kilosResult = $_POST['kilos'];
+
+	$poundsResult = $kilosResult * 2.2;
+
+	echo "<p>" . $kilosResult . " kilos equals "  . $poundsResult . " pounds.</p>";
+}
+else
+{
+	echo "<p>Nothing to do.</p>";
+}
+?>
+<p><a href="KiloPoundForm.html">Go back.</a></p>
 </body>
 </html>
