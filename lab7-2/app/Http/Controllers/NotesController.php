839823110a210f41da4998075881538d4021f743
mayfaroukeg <mayfaroukeg@gmail.com>
2016-11-21 12:17:25 -0400

    end of video 10

---
+++ lab7-2/app/Http/Controllers/NotesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Card;
+use App\Note;
+
+
+use Illuminate\Http\Request;
+
+//use App\Http\Requests;
+
+//use App\Http\Controllers\Controller;
+
+class NotesController extends Controller
+{
+    public function store(Request $request, Card $card)
+    {
+
+        $card->addNote(
+
+
+            new Note($request->all())
+        );
+        return back();
+    }
+
+
+    public function edit(Note $note)
+    {
+
+
+        return view ('notes.edit', compact('note'));
+    }
+
+
+
+    public function update(Request $request, Note $note)
+    {
+
+
+       $note->update($request->all());
+        return back();
+    }
+}
